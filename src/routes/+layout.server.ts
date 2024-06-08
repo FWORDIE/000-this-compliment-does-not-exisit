d8ca5588bf2c01f3017b685a395d256d64771757
Fred Wrodie <fred@mildlyupset.com>
2024-06-08 19:17:13 +0200

    updates

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,16 @@
 import { datoToken } from "$env/static/private";
 
 export async function load({ params }) {
-    const query = `
+    let responseArray = [];
+    let query = `
         query {
-            allCompliments(first: "100") {
-              text
-              audio {
-                url
+            _allComplimentsMeta {
+                count
               }
-            }
           }
     `;
 
-    const response = await fetch("https://graphql.datocms.com/", {
+    const response1 = await fetch("https://graphql.datocms.com/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -20,11 +18,36 @@ export async function load({ params }) {
         },
         body: JSON.stringify({ query }),
     });
-    const json = await response.json();
 
-    const returnable = json.data;
-    console.log(returnable.allCompliments.length)
+    const jsontotal = await response1.json();
+    while (responseArray.length < jsontotal.data._allComplimentsMeta.count) {
+
+        query = `
+        query {
+            allCompliments(skip: "${responseArray.length}", first: "100") {
+              text
+              audio {
+                url
+              }
+            }
+          }
+    `;
+        const response = await fetch("https://graphql.datocms.com/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${datoToken}`,
+            },
+            body: JSON.stringify({ query }),
+        });
+        const json = await response.json();
+
+        const returnable = json.data;
+        // console.log(returnable.allCompliments.length);
+        responseArray = [...responseArray, ...returnable.allCompliments]
+    }
+
     return {
-        data: returnable,
+        data: responseArray,
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
     let play = false;
     let compliment = {};
     onMount(async () => {
-        let compliments = data.data.allCompliments;
+        let compliments = data.data
         compliment = compliments[Math.floor(Math.random() * compliments.length)];
         // var sound = new Howl({
         //     src: [audio],
@@ -18,14 +18,19 @@
     });
     let duration = 0;
     let done = true;
-    const playVoice = async  () => {
-        done = false
-        let compliments = data.data.allCompliments;
+    let fade = false;
+    const playVoice = async () => {
+        done = false;
+        fade = true;
+
+        let compliments = data.data;
         compliment = compliments[Math.floor(Math.random() * compliments.length)];
         var audio = new Audio(compliment.audio[0].url);
         audio.addEventListener("loadedmetadata", async (event) => {
-            await delay(300)
-            text = compliment.text
+            await delay(300);
+            fade = false;
+
+            text = compliment.text;
             duration = audio.duration;
             play = true;
             audio.play();
@@ -34,30 +39,44 @@
             finished();
         });
     };
-    $: text = ''
-    const finished = async  () => {
-
-        await delay(300)
-        doneOne = true
+    $: text = "";
+    const finished = async () => {
+        await delay(300);
+        doneOne = true;
         done = true;
     };
 
-    let doneOne =  false
+    let doneOne = false;
+    let aboutSpace = false;
 </script>
 
 <div class="holder">
     {#if compliment}
-        <div class="textHolder">
+        <div class="textHolder" class:fade>
             {#if play}
-                <Typewriter interval={(duration * 1000) / text.length - 5} cursor={false} >
-                    <h1>{text}</h1>
+                <Typewriter interval={(duration * 1000) / text.length - 5} cursor={false}>
+                    <p class="title">{text}</p>
                 </Typewriter>
             {:else}
-            <h1>❤️</h1>
+                <p class="title">❤️</p>
             {/if}
-            <button on:click={playVoice} disabled={!done}>{doneOne ?  'Give me another' : 'Give me a compliment'}</button>
+            <button on:click={playVoice} disabled={!done} class:hidden={!done}>{doneOne ? "Give me another" : "Give me a compliment"}</button>
         </div>
     {/if}
+    {#if aboutSpace}
+        <div class="about">
+            <p class="title">❤️</p>
+            <p><b>This Compliment Does Not Exist</b></p>
+            <p>
+                Virtual Hugs from your friends at <a href="https://openai.com/" target="_blank">OpenAi</a> and
+                <a href="https://elevenlabs.io" target="_blank">ElevenLabs</a>
+            </p>
+            <p>Made by <a href="https://www.fredwordie.com/">Fred Wordie</a></p>
+        </div>
+    {/if}
+    <button class="aboutButton" on:click={() => (aboutSpace = !aboutSpace)}>
+        {aboutSpace ? "x" : "?"}
+    </button>
 </div>
 
 <style lang="scss">
@@ -67,6 +86,16 @@
         justify-content: center;
         align-items: center;
         height: 100dvh;
+        background-color: var(--white);
+        color: var(--black);
+
+        .title {
+            color: var(--black);
+            opacity: 1;
+            font-size: var(--font-size-lg);
+            transition: all 300ms;
+        }
+
         .textHolder {
             min-height: 70%;
             width: 70%;
@@ -75,12 +104,48 @@
             flex-direction: column;
             justify-content: center;
             align-items: center;
+
+            &.fade p {
+                opacity: 0;
+            }
+        }
+
+        .about {
+            position: absolute;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: var(--white);
+            width: 100%;
+            color: var(--black);
+            p {
+                margin-block: var(--halfPadding);
+            }
+            a {
+                color: var(--black);
+            }
+        }
+        .aboutButton {
+            position: absolute;
+            top: var(--padding);
+            right: var(--padding);
+            z-index: 10;
+            text-decoration: none;
         }
         button {
             border: 0px;
             background: none;
             text-decoration: underline;
+            font-size: var(--base-font);
             cursor: pointer;
+            opacity: 1;
+            transition: opacity 100ms;
+            &.hidden {
+                opacity: 0;
+                cursor: default;
+            }
         }
     }
 </style>
